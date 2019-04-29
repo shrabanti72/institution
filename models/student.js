49a71e54729f1ab472f993c8c7b51786809965c3
shrabanti72 <42089199+shrabanti72@users.noreply.github.com>
2019-04-30 01:19:46 +0530

    Update student.js

--- models/student.js
+++ models/student.js
@@ -4,7 +4,6 @@ var mongoose = require("mongoose"),
 var UserSchema = new mongoose.Schema({
     username: String,
     email:String,
-    password: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
